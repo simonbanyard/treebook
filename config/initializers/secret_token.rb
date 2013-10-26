19c94fdb47ff8fd6992c47c5fe48cea0f8b176ed
simonbanyard <simon.banyard@gmail.com>
2013-10-26 17:37:48 +0100

    Created a Rails app and added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '7d0e36e0077e5ffd5964764006cbe9b5d5e170940d7c6252611afb62cffd403916d91b64274c6735966a44b62e7178c4476f82e2104b0e19e1ad9c566922a6b5'
---
+++ vendor/bundle/gems/json-1.8.1/ext/json/ext/generator/siteconf20131026-3534-rkbvth.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131026-3534-9sy2mc"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
